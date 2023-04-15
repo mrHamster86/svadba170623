c74eb3e0c2295980f30f2cc1c4db0084ae39ded2
Evgeniy <39721548+mrHamster86@users.noreply.github.com>
2023-04-16 00:41:45 +0300

    Add favicon (#7)
    * Добавил поделяшки, фавиконку, таймер на форму
    
    * Исправил поделяшку
    
    ---------
    
    Co-authored-by: mr.hamster <mrhamster1986@mail.ru>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,12 +10,12 @@ export default function Document() {
         <link rel="icon" href="img/favicon/32.svg" type="image/svg+xml" />
         <link rel="manifest" href="manifest.webmanifest" />
 
-        <meta property="og:title" content="Приглашение | Wedding-day" />
+        <meta property="og:title" content="Приглашаем вас на нашу свадьбу" />
         <meta property="og:image" content="https://svadba170623.ru/img/micro.jpg" />
         <meta property="og:image:width" content="2500" />
         <meta property="og:image:height" content="1330" />
         <meta property="og:url" content="https://svadba170623.ru" />
-        <meta property="og:site_name" content="Приглашаем вас на нашу свадьбу. Будем радоваться вместе с вами в наш счастливый день! Ждем вас 17 июня!" />
+        <meta property="og:description" content="Будем радоваться вместе с вами в наш счастливый день! Ждем вас 17 июня!" />
         <meta property="og:type" content="website" />
       </Head>
       <body>
